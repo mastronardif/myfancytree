2bcae91d9a195322d3efa16f6697481e5068ccbc
Frank Mastronardi <mastronardif@gmail.com>
2016-07-10 19:07:02 -0400

    added react stuff

--- server/server.js
+++ server/server.js
@@ -1,12 +1,18 @@
 'use strict';
 
-var express   = require('express');
-var path      = require('path');
-
+var express    = require('express');
+var fs = require('fs');
+var path       = require('path');
+var bodyParser = require('body-parser');
+var winston    = require('winston');
+const util = require('util');
 /**
  * Create the Express application.
  */
 var app = express();
+
+var COMMENTS_FILE = path.join(__dirname, 'comments.json');
+
 //app.use(express.static(path.join(__dirname, '/client/src')));
 //app.use('/js', express.static(__dirname + 'client/js/'));
 //app.use('/js',express.static('./client/js'));
@@ -14,6 +20,59 @@ var app = express();
 app.use('/src', express.static(__dirname + '/client/src/'));
 console.log(__dirname+'/client/src');
 
+app.use('/scripts',express.static('./client/js'));
+app.use('/css',express.static('./client/css'));
+app.use(express.static('client/react/'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/api/comments', function(req, res) {
+    //console.error('inside: /api/comments');
+    //winston.log('info', 'Hello distributed log files!');
+    var ddd = new Date();
+    //winston.info('inside: /api/comments', ddd.toLocaleTimeString());   
+    winston.log('info', '%s %s', ddd.toLocaleTimeString(), '/api/comments');
+    //winston.log('info', 'test message %d', 123);
+    
+  fs.readFile(COMMENTS_FILE, function(err, data) {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    res.json(JSON.parse(data));
+  });
+});
+
+app.post('/api/comments', function(req, res) {
+    var ddd = new Date();
+    winston.log('info', '%s %s', ddd.toLocaleTimeString(), '/api/comments');   
+    winston.log('info', '%s', 
+        util.format('%s: %j', ddd.toLocaleTimeString(), req.body));
+   
+  fs.readFile(COMMENTS_FILE, function(err, data) {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    var comments = JSON.parse(data);
+    // NOTE: In a real implementation, we would likely rely on a database or
+    // some other approach (e.g. UUIDs) to ensure a globally unique id. We'll
+    // treat Date.now() as unique-enough for our purposes.
+    var newComment = {
+      id: Date.now(),
+      author: req.body.author,
+      text: req.body.text,
+    };
+    comments.push(newComment);
+    fs.writeFile(COMMENTS_FILE, JSON.stringify(comments, null, 4), function(err) {
+      if (err) {
+        console.error(err);
+        process.exit(1);
+      }
+      res.json(comments);
+    });
+  });
+});
 
 app.route('/:id')
 //p.route('/')
@@ -32,6 +91,21 @@ app.route('/:id')
 
   app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3002);
   app.set('ip', process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1');
+  
+  app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+// Additional middleware which will set headers that we need on each request.
+app.use(function(req, res, next) {
+    // Set permissive CORS header - this allows this server to be used only as
+    // an API server in conjunction with something like webpack-dev-server.
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Disable caching so we'll always get the latest comments.
+    res.setHeader('Cache-Control', 'no-cache');
+    next();
+});
+
 
 //app.on('stormpath.ready',function(){
 app.listen(app.get('port'), app.get('ip'), function () {
